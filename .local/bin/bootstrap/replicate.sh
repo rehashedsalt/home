163e8568a37b2c08b6b329d3833c6c85efbbc225
Salt <rehashedsalt@cock.li>
2018-01-17 12:16:19 -0600

    Bootstrap: Fix requiring valid SSH keys to clone repository It seemed like a good idea at the time, but it was completely untested up until recently

--- .local/bin/bootstrap/replicate.sh
+++ .local/bin/bootstrap/replicate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 tempfolder="/tmp/dots$(uuidgen)"
-git clone --recursive --separate-git-dir="$HOME/.dotfiles" git@github.com:rehashedsalt/home.git $tempfolder
+git clone --recursive --separate-git-dir="$HOME/.dotfiles" https://github.com/rehashedsalt/home.git $tempfolder
 rsync -rvl --exclude ".git" $tempfolder/ $HOME/
 rm -r $tempfolder
 git submodule update --init --recursive --git-dir="$HOME/.dotfiles" --work-tree="$HOME" $HOME/
