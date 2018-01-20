b6ac612d1bdb11956a4e59941161730f09944df3
Salt <rehashedsalt@cock.li>
2018-01-20 13:56:55 -0600

    Polybar: Switch to startup script

---
+++ .config/polybar/start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# start.sh
+# Copyright (C) 2018 salt <salt@iridium>
+#
+# Distributed under terms of the MIT license.
+#
+
+# Which WM are we using?
+if $(pgrep i3); then
+	export PB_WM_RESTACK=i3
+fi
+if $(pgrep bspwm); then
+	export PB_WM_RESTACK=bspwm
+fi
+
+# Iterate through monitors and spawn bars on each
+# Starting with the primary monitor...
+export PB_MONITOR=$(xrandr -q | awk '/\<primary\>/{print $1}')
+if [[ $PB_MONITOR == "" ]]; then
+	exit 1
+fi
+polybar -r primary-top&
+# ...and then moving on to secondaries, if we have them
+# Alright, now this looks *really* bad, but there's no way in hell you can get
+# me to attempt to solve this with regex. Fuck regex. It starts more problems
+# than it solves
+export secondary_monitors=$(xrandr -q | grep ' connected' | grep -v 'primary' | awk '{print $1}')
+if [[ $PB_MONITOR == "" ]]; then
+	exit 0
+fi
+for monitor in secondary_monitors; do
+	PB_MONITOR=$monitor polybar -r secondary-top
+done
+
+disown
+exit
